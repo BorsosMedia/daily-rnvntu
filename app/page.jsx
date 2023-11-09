3beb721ced09d791313bc9a26db8edaf58eaea98
Luis Landaeta <luis.landaeta24@gmail.com>
2023-11-09 16:57:45 -0400

    Change home texts

--- app/page.jsx
+++ app/page.jsx
@@ -13,21 +13,21 @@ export default function Home() {
     <main className="main_home">
       <header className="colTwo">
         <Image src={logo} alt="logo" className="header__logo" />
-        <h6 className="text--body text--desc">2023 USA</h6>
+        <h6 className="text--body text--desc">FL, USA 2023</h6>
       </header>
       <section className="v-align-gap-1 align-center">
         <p className="paragraph paragraph--grey">
-          Hardcore training has never been easier
+          It&apos;s not about luck. It&apos;s about persistence.
         </p>
         <h1 className="heading text-center">
           DAILY <span className="primary-accent setAnimation">RNVNTU</span>
         </h1>
-        <h5 className="subheading">A very srs daily fitness program</h5>
+        <h5 className="subheading">Your transformation starts here</h5>
       </section>
       <section className="v-align-gap-1 align-center">
         <NavigationButton text="Sign Up" to="/register" primary={true} />
         <NavigationButton text="Login" to="/login" />
-        <span className="text--body">DAILY RNVNTU</span>
+        <span className="text--body">Ready to start?</span>
       </section>
     </main>
   );
