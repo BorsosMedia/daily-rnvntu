ab4c6910cdc16e785ceec104a1d0be0f515ba6f9
Luis Landaeta <luis.landaeta24@gmail.com>
2023-11-29 17:03:45 -0400

    Add cta text on home

--- app/page.jsx
+++ app/page.jsx
@@ -26,6 +26,15 @@ export default function Home() {
       <section className="v-align-gap-1 align-center">
         <NavigationButton text="Sign Up" to="/register" primary={true} />
         <NavigationButton text="Login" to="/login" />
+        <p
+          className="text--desc text-center paragraph paragraph--grey"
+          style={{ margin: "0 10vw" }}
+        >
+          Join me in my daily real-time workouts, updated weekly, for just 20
+          cents a day. Experience my personalized training that fits your
+          schedule and goals. Elevate your fitness, one affordable step at a
+          time. Results guaranteed!
+        </p>
         <span className="text--body">Ready to start?</span>
       </section>
     </main>
